5ddce893b5a4eb76422c0c3e4b81d519a264b413
Mikhail Movchan <m-movcha@e1z2r3p16.42.us.org>
2020-02-20 17:43:35 -0800

    add

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,16 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   memset.c                                           :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: m-movcha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/18 23:53:22 by m-movcha          #+#    #+#             */
-/*   Updated: 2020/02/18 23:54:29 by m-movcha         ###   ########.fr       */
+/*   Created: 2020/02/20 11:57:27 by m-movcha          #+#    #+#             */
+/*   Updated: 2020/02/20 13:07:23 by m-movcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void *	memset(void *b, int c, size_t len)
-{
+#include <stdio.h>
 
+void    *ft_memmove(void *dst, const void *src, size_t len)
+{
+	size_t i;
+	unsigned char *dest;
+	unsigned char *source;
+	
+	dest = dst;
+	source = src;
+	
+	i = 0;
+	while (i < len && *dest)
+	{
+		*dest = *source;
+		dest++;
+		source++;
+		i++;
+	}
 }
