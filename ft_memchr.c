119eca7562302ea5cd3840542974b3d1aa0db9f1
Mikhail Movchan <m-movcha@e1z2r3p16.42.us.org>
2020-02-20 18:19:24 -0800

    ft_memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,34 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: m-movcha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/20 17:12:51 by m-movcha          #+#    #+#             */
-/*   Updated: 2020/02/20 17:45:48 by m-movcha         ###   ########.fr       */
+/*   Created: 2020/02/20 18:02:18 by m-movcha          #+#    #+#             */
+/*   Updated: 2020/02/20 18:18:45 by m-movcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_memcmp(const void *s1, const void *s2, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t			num;
-
-	unsigned char 	*str1;
-	unsigned char 	*str2;
+	unsigned char 	*str;
 
 	num = n + 1;
-
-	if (num != 0)
+	str = s;
+	if (str != NULL)
 	{
-		str1 = s1;
-		str2 = s2;
-
 		while (--n != 0)
 		{
-			if (*str1++ != *str2++)
-				return (*--str1 - *--str2);
+			if (*str == c)
+				return (void *)str;
+			str++;
 		}
 	}
-	return (0);
+	return (NULL);
+
 }
