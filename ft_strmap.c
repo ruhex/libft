1c487b84ba86ddaa1c126ae0858824f597fe12f3
Mikhail Movchan <m-movcha@e1z3r10p6.42.us.org>
2020-02-26 19:44:16 -0800

    ft_strmap

--- ft_strmap.c
+++ ft_strmap.c
@@ -6,24 +6,25 @@
 /*   By: m-movcha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/26 18:32:00 by m-movcha          #+#    #+#             */
-/*   Updated: 2020/02/26 18:39:17 by m-movcha         ###   ########.fr       */
+/*   Updated: 2020/02/26 19:43:38 by m-movcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char * ft_strmap(char const *s, char (*f)(char))
+char * ft_strmap(char const *s, char (*f)(char))
 {
 	char *tmp;
+	char *p;
 
 	tmp = ft_strnew(ft_strlen(s));
 
-	if (tmp)
+	if (s)
 	{
-		while (tmp)
-		{
-			(*f)*(tmp++);
-		}
+		while (*s)
+			*(tmp++) = (*f)((*s++));
+		*(tmp++) = '\0';
+		tmp = p;
 		return (tmp);
 	}
 	return (NULL);
