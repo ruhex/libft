b258b164d3c9c8721e5071e672e707e25dd73c90
Mikhail Movchan <m-movcha@e1z3r10p6.42.us.org>
2020-03-01 22:59:09 -0800

    fix ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: m-movcha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/20 18:02:18 by m-movcha          #+#    #+#             */
-/*   Updated: 2020/03/01 21:41:58 by m-movcha         ###   ########.fr       */
+/*   Updated: 2020/03/01 22:58:44 by m-movcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	size_t			num;
 	const char		*str;
 
-	num = n + 1;
+	num = n;
 	str = (const char *)s;
-	while (--n != 0)
+	while ((int)--n > -1)
 	{
 		if (*str == c)
 			return (void *)str;
